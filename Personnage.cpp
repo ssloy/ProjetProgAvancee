1691e3fc56ab81309fd9cc52070dae38ca059058
LucasVillaume <lvillaume17@gmail.com>
2020-09-23 11:48:31 +0200

    Implémentation fonction deplacer

---
+++ Personnage.cpp
@@ -0,0 +1,17 @@
+#include "Personnage.h"
+#include <iostream>
+using namespace std;
+
+Personnage::Personnage(): vie(3),pos_x(0),pos_y(0),vitesse(5),nom("Dummy"){
+    
+}
+
+void deplacer(char dir){
+    switch(dir):
+        case q:
+            pos_x -= vitesse;
+            break;
+        case d:
+            pos_x += vitesse;
+            break;
+}
