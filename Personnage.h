dbc7824ad95697116f0242876b8076ace717b2ed
LucasVillaume <lvillaume17@gmail.com>
2020-09-23 11:34:36 +0200

    Initialisation objet Personnage

---
+++ Personnage.h
@@ -0,0 +1,23 @@
+#ifndef DEF_PERSONNAGE
+#define DEF_PERSONNAGE
+
+
+class Personnage{
+    public:
+    
+    Personnage(); //Constructeur
+    void deplacer(char d);
+    void sauter();
+    void recevoirDegats();
+    bool dansAir();
+    bool estVivant();
+    
+    private:
+    
+    int vie;
+    int pos_x;
+    int pos_y;
+    int vitesse;
+    //Capacite capacite;
+        
+};
